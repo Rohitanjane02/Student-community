8f7f24476382130231fc40bb4549684294398359
Rohit Anjane <rohitanjane999@gmail.com>
2022-11-03 20:13:07 +0530

    resolving url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey = 'qgtk9ttyha7j';
+const apiKey = '89tu45n725ne';
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
